2226c49c53fb111f0c08c4044812ca9ea2f3495a
SaiReddy <saireddy@SaiReddys-iMac.local>
2017-01-14 12:52:38 +0530

    Finished learning need to check it again

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by SaiReddy on 13/01/17.
+//  Copyright © 2017 SaiReddy. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+   
+}
+
